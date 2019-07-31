b6eb811fef4f07bccce36bd2f9950847490be954
Roman <r.panchenkoWork@gmail.com>
2019-07-31 12:13:51 +0300

    change F to C

--- utils/forecast.js
+++ utils/forecast.js
@@ -9,7 +9,7 @@ const forecast = (latitude, longitude, callback) => {
         } else if (body.error) {
             callback('Unable to find location', undefined)
         } else {
-            callback(undefined, `${body.daily.data[0].summary} It is currently ${body.currently.temperature} degress out. There is a ${body.currently.precipProbability}% chance of rain.`)
+            callback(undefined, `${body.daily.data[0].summary} It is currently ${Math.round((body.currently.temperature -32) * 5/9)} degress out. There is a ${body.currently.precipProbability}% chance of rain.`)
         }
     })
 }
